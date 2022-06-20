58e1da53e670d3c5a0768ff421e06c78f0e54e05
renangoms <92700912+renangoms@users.noreply.github.com>
2022-06-20 19:25:16 -0400

    Add files via upload

---
+++ fe/src/pages/Home/index.js
@@ -0,0 +1,54 @@
+import { Link } from 'react-router-dom';
+
+import {
+  Container, Header, ListContainer, Card, InputSearchContainer,
+} from './styles';
+
+import arrow from '../../assets/images/icons/arrow.svg';
+import edit from '../../assets/images/icons/edit.svg';
+import trash from '../../assets/images/icons/trash.svg';
+
+export default function Home() {
+  return (
+    <Container>
+      <InputSearchContainer>
+        <input type="text" placeholder="Pesquisar contato..." />
+      </InputSearchContainer>
+
+      <Header>
+        <strong>3 contatos</strong>
+        <Link to="/new">Novo Contato</Link>
+      </Header>
+
+      <ListContainer>
+        <header>
+          <button type="button">
+            <span>Nome</span>
+            <img src={arrow} alt="Arrow" />
+          </button>
+        </header>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Renan Goms</strong>
+              <small>instagram</small>
+            </div>
+            <span>renan@gmail.com</span>
+            <span>(67) 99237-3050</span>
+          </div>
+
+          <div className="actions">
+            <Link to="/edit/123">
+              <img src={edit} alt="Edit" />
+            </Link>
+            <button type="button">
+              <img src={trash} alt="Delete" />
+            </button>
+          </div>
+        </Card>
+
+      </ListContainer>
+    </Container>
+  );
+}
